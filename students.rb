abdf94ce639f5319ffea7469fb283ca87bf3cbc0
m05e5 <calebmoses1111@gmail.com>
2021-10-11 17:01:27 +0100

    fix errors

--- students.rb
+++ students.rb
@@ -2,7 +2,7 @@ require_relative './person'
 
 # this is the class student
 class Student < Person
-  def initialize(age:, classroom:, name: 'unknown', parent_permission: true)
+  def initialize(age, classroom, name = 'unknown', parent_permission = true)
     super(age: age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require_relative './person'
 
 # this is the class teacher
 class Teacher < Person
-  def initialize(age:, specialization:, parent_permission: true, name: 'unknown')
+  def initialize(age, specialization, parent_permission = true, name = 'unknown')
     super(age: age, parent_permission: parent_permission, name: name)
     @specialization = specialization
   end
