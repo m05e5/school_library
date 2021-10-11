055d91634c3b10de9cf80fd61b3104c7a6f02496
m05e5 <calebmoses1111@gmail.com>
2021-10-11 17:14:21 +0100

    fix error

--- person.rb
+++ person.rb
@@ -1,11 +1,9 @@
-# frozen_string_literal: true
-
 # this is the class person
 class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, parent_permission= true, name= 'Unknown')
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require_relative './person'
 
 # this is the class teacher
 class Teacher < Person
-  def initialize(age, specialization, parent_permission = true, name = 'unknown')
+  def initialize(age, specialization, name = 'unknown', parent_permission: true)
     super(age: age, parent_permission: parent_permission, name: name)
     @specialization = specialization
   end
