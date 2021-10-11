3afe37c0b87c0271a0278f16ac0bc8bf9ab301b6
m05e5 <calebmoses1111@gmail.com>
2021-10-11 16:57:04 +0100

    fix error

--- person.rb
+++ person.rb
@@ -5,7 +5,7 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age:, parent_permission: true, name: 'Unknown')
+  def initialize(age, parent_permission= true, name= 'Unknown')
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -16,7 +16,7 @@ class Person
     false unless @age >= 18
     true
   end
-  private :of_age
+  private :of_age?
 
   def can_use_services
     true if of_age? || @parent_permission
