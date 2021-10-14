6a5b8c7ca3ee952c8abb37df191d0d6938142d47
m05e5 <calebmoses1111@gmail.com>
2021-10-14 19:51:45 +0100

    removed start options from initialize for ruby best practice

--- main.rb
+++ main.rb
@@ -4,14 +4,6 @@ require_relative './teacher'
 require_relative './rental'
 class App
   def initialize()
-    @start = 'Please choose an option by entering a number
-    1 - List all books
-    2 - List all people
-    3 - Create a person
-    4 - Create a book
-    5 - Create a rental
-    6 - List all rentals for a given person id
-    7 - exit'
     @books = []
     @person = []
     @rentals = []
@@ -97,6 +89,14 @@ class App
   end
 
   def redoo
+    @start = 'Please choose an option by entering a number
+    1 - List all books
+    2 - List all people
+    3 - Create a person
+    4 - Create a book
+    5 - Create a rental
+    6 - List all rentals for a given person id
+    7 - exit'
     puts @start
     @num = gets.chomp
   end
